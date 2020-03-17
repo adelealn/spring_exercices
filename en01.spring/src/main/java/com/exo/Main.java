f5462879a97e7db79733d75a124145e3048367c7
Adele <adele.alloin@yahoo.fr>
2020-03-17 12:48:16 +0100

    Exercice 1

--- en01.spring/src/main/java/com/exo/Main.java
+++ en01.spring/src/main/java/com/exo/Main.java
@@ -1,55 +1,37 @@
 package com.exo;
 
+// Attention aux import, nous sommes en log4j2
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+// Import Spring
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Exemple.
- */
+
 public final class Main {
-	private static final Logger LOG = LogManager.getLogger();
+  private final static Logger LOG = LogManager.getLogger();
 
-	/**
-	 * Constructeur.
-	 */
-	private Main() {
-		super();
-		Main.LOG.error("Ne pas utiliser le constructeur");
-	}
-
-	/**
-	 * Charge un fichier Spring.
-	 *
-	 * @param args
-	 *            ne sert a rien
-	 */
-	public static void main(String[] args) {
-		Main.LOG.debug("-- Debut -- ");
-		ClassPathXmlApplicationContext appContext = null;
-		try {
-			// Chargement du fichier
-			appContext = new ClassPathXmlApplicationContext("spring/mesBeans.xml");
-
-			// Recuperation de notre instance de client
-			// Client cl1 = (Client) appContext.getBean("client1");
-			// Affichage
-			// Main.LOG.debug(cl1.toString());
-		} catch (BeansException e) {
-			Main.LOG.fatal("Erreur", e);
-			System.exit(-1);
-		} finally {
-			if (appContext != null) {
-				appContext.close();
-			}
-			// On peut aussi faire :
-			// appContext.registerShutdownHook();
-			// Juste apres la creation du context, de cette manière il sera
-			// detruit automatiquement à la fin du programme
-		}
-
-		Main.LOG.debug("-- Fin -- ");
-		System.exit(0);
-	}
+  public static void main(String[] args) {
+    Main.LOG.debug("-- Debut -- ");
+    // Declaration du contexte Spring
+    ClassPathXmlApplicationContext appContext = null;
+    try {
+      // Chargement du fichier Spring
+      appContext = new ClassPathXmlApplicationContext("classpath:spring/mesBeans.xml");
+      // Récupération de notre instance de client
+      Client cl1 = (Client) appContext.getBean("monId");
+      // Affichage
+      Main.LOG.debug(cl1.toString());
+    } catch (Exception e) {
+      Main.LOG.fatal("Erreur", e);
+      System.exit(-1);
+    } finally {
+      // Quoi qu'il arrive fermeture du contexte Spring
+      if (appContext != null) {
+        appContext.close();
+      }
+    }
+    Main.LOG.debug("-- Fin -- ");
+    System.exit(0);
+  }
 }
