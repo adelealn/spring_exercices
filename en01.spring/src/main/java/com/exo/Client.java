d9a8702eb6929536ae0768b235a9c5d6e955db34
Adele <adele.alloin@yahoo.fr>
2020-03-17 16:56:08 +0100

    Début exercice 5

--- en01.spring/src/main/java/com/exo/Client.java
+++ en01.spring/src/main/java/com/exo/Client.java
@@ -1,12 +1,15 @@
 package com.exo;
 
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Client {
 
 	private String nom, prenom;
 	private Integer age;
 	private java.util.List<Adresse> adresses;
+	private final static Logger LOG = LogManager.getLogger();
 	
 	public Client() {
 		
@@ -19,7 +22,15 @@ public class Client {
 		this.age = age;
 		this.adresses = adresses;
 	}
-
+	
+	
+	public void initialiser() {
+		
+	}
+	
+	public void detruire() {
+		
+	}
 
 	public String getNom() {
 		return nom;
